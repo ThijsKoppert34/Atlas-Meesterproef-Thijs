8cadf3339b2293b6d7fde8e4188f432b9bf010c6
Christian van Drunen <christian.van.drunen@hva.nl>
2025-06-02 10:01:53 +0200

    start animation

--- client/detailPage.js
+++ client/detailPage.js
@@ -1,14 +1,53 @@
-console.log("Hello world");
-
 gsap.registerPlugin(ScrollTrigger);
 
+ScrollTrigger.create({
+  trigger: ".scene1",
+  start: "top top",
+  end: "+=200", // length of pin
+  pin: true,
+  onEnter: () => lockScroll(),
+  onLeave: () => unlockScroll(),
+  markers: "true"
+});
+
 gsap.to(".box", {
- scale:3, // Move it completely off-screen upwards
+ scale:20,
+  y: () => window.innerHeight - 200,
   scrollTrigger: {
     trigger: ".box",
-    start: "10",     // When the top of .box hits the top of the viewport
-    end: "+=300",         // Animate over 300px of scrolling
+    start: 100,
+    end: 200,
     // pin: true,
-    scrub: true,          // Smooth scroll-linked animation
+    scrub: true,
+    // markers: "true"
+  }
+});
+
+
+gsap.to(".skyline1", {
+ scale:.1,
+  x: -800,
+  y: 200,
+  scrollTrigger: {
+    trigger: ".box",
+    start: 100,
+    end: 200,
+    // pin: true,
+    scrub: true,
+    markers: "true"
+  }
+});
+
+gsap.to(".skyline2", {
+ scale:.1,
+  x: 800,
+  y: 200,
+  scrollTrigger: {
+    trigger: ".box",
+    start: 100,
+    end: 200,
+    // pin: true,
+    scrub: true,
+    markers: "true"
   }
 });
