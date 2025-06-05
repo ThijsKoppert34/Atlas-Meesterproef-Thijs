3d411fe0f7b6bb8ebae3dd89f43efbaddc32e0b4
Christian van Drunen <christian.van.drunen@hva.nl>
2025-06-05 10:28:53 +0200

    scene 1 updated

--- client/detailPage.js
+++ client/detailPage.js
@@ -1,131 +1,27 @@
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.create({
-  trigger: ".scene-1",
+let tlScene1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".scene-1",
   start: "top top",
-  end: "+=250",
+  end: "+=800vh",
   pin: true,
-  // markers: "true"
-});
-
-gsap.to(".adress", {
-  y: -1000,
-  scrollTrigger: {
-    trigger: ".adress",
-    start: 95,
-    end: 125,
-    scrub: true,
-    markers: "true"
+  pinSpacing: true,
+  scrub: 2,
+  markers: true
   }
-});
-
-gsap.to(".family", {
-  y: -1000,
-  scrollTrigger: {
-    trigger: ".family",
-    start: 95,
-    end: 125,
-    scrub: true,
-    markers: "true"
-  }
-});
-
-gsap.to(".cloud-1", {
-  x: 500,
-  scrollTrigger: {
-    trigger: ".cloud-1",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".cloud-2", {
-  x: 500,
-  scrollTrigger: {
-    trigger: ".cloud-2",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".cloud-3", {
-  x: -50,
-  scrollTrigger: {
-    trigger: ".cloud-3",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".skyline-1", {
- scale:.1,
-  x: -800,
-  y: 200,
-  scrollTrigger: {
-    trigger: ".skyline-1",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".skyline-2", {
- scale:.1,
-  x: 800,
-  y: 200,
-  scrollTrigger: {
-    trigger: ".skyline-2",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".tree-1", {
- scale:3,
-  x: -2000,
-  scrollTrigger: {
-    trigger: ".tree-1",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-
-gsap.to(".tree-2", {
- scale:3,
-  x: 2000,
-  scrollTrigger: {
-    trigger: ".tree-2",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".bike", {
- scale:10,
-  x: -2250,
-  scrollTrigger: {
-    trigger: ".bike",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
-gsap.to(".house", {
- scale:20,
-  y: () => window.innerHeight - 200,
-  scrollTrigger: {
-    trigger: ".box",
-    start: 100,
-    end: 250,
-    scrub: true,
-  }
-});
-
+})
+  
+tlScene1
+.to(".adress", { y: "-100vh", duration: 1}, 0)
+.to(".family",{ y: "-100vh", duration: 1}, 0)
+.to(".cloud-1",{ x: "50vw", duration: 1}, 0)
+.to(".cloud-2",{ x: "50vw", duration: 1}, 0)
+.to(".cloud-3",{ x: "-5vw", duration: 1}, 0)
+.to(".skyline-1",{ x: "-100vw", y: "25vh", scale: .1, duration: 3}, .2)
+.to(".skyline-2",{ x: "100vw", y: "25vh", scale: .1, duration: 3}, .2)
+.to(".tree-1",{ x: "-250vw", scale: 10, duration: 3}, .2)
+.to(".tree-2",{ x: "250vw", scale: 10, duration: 3}, .2)
+.to(".bike", { x: "-175vw", scale: 20, duration: 3}, .2)
+.to(".house", { scale: 40, duration: 3}, .2)
+.to(".scene-1-transition", {opacity: 1, }, 1)
